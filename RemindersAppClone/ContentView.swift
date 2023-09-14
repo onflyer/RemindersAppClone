530b001138b1e31cba9353bc08de28e5fea4fc77
onflyer <salkaorama@gmail.com>
2023-09-15 00:02:43 +0200

    Initial Commit

---
+++ RemindersAppClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemindersAppClone
+//
+//  Created by Aleksandar Milidrag on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemindersAppClone/RemindersAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppCloneApp.swift
+//  RemindersAppClone
+//
+//  Created by Aleksandar Milidrag on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
