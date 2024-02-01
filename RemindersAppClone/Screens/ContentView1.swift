7619c75380898abe254505675287e504103ea28b
onflyer <salkaorama@gmail.com>
2024-02-01 22:19:42 +0100

    fixed almost everything

--- RemindersAppClone/Screens/ContentView1.swift
+++ RemindersAppClone/Screens/ContentView1.swift
@@ -15,34 +15,22 @@ struct ContentView1: View {
     
     static var now: Date { Date.now }
     
-    
     var searchedReminders: [Reminder] {
         return reminders.filter {
-            $0.title.contains(search)
+            $0.title.localizedStandardContains(search)
         }
     }
-    
     var todayReminders: [Reminder] {
         return reminders.filter {
             $0.reminderDate!.isToday
         }
     }
-    
     @Query(filter: #Predicate<Reminder> { $0.isCompleted == true
-        
     }) private var completedReminders: [Reminder]
     
     @Query(filter: #Predicate<Reminder> { (($0.reminderDate != nil || $0.reminderTime != nil) && $0.isCompleted == false)
-        
     }) private var scheduledReminders: [Reminder]
     
-    // MARK: PREDICATE NOT WORKING WITH DATES CURRENTLY
-//    @Query(sort: [SortDescriptor(\Reminder.reminderDate )]) private var todayReminders: [Reminder]
-    
-//    @Query(filter: #Predicate<Reminder> { $0.reminderDate!.isToday
-//    }) private var todayReminders: [Reminder]
-    
- 
     @State var search: String = ""
     @State private var searching: Bool = false
     @State private var isPresented: Bool = false
@@ -54,23 +42,20 @@ struct ContentView1: View {
         NavigationStack {
             VStack {
                 ScrollView {
-                
                     HStack {
-
+                        
                         NavigationLink {
-                          ReminderListView(reminders: todayReminders)
+                            ReminderListView(reminders: todayReminders)
                         } label: {
                             ReminderStatsView(icon: "calendar", title: "Today", count: reminderStatsValues.todayCount)
                         }
                         
                         NavigationLink {
-                          ReminderListView(reminders: reminders)
-
+                            ReminderListView(reminders: reminders)
+                            
                         } label: {
                             ReminderStatsView(icon: "tray.circle.fill", title: "All", count: reminderStatsValues.allCount, iconColor: .red)
                         }
-
-                        
                         
                     }
                     .padding(.horizontal)
@@ -78,17 +63,17 @@ struct ContentView1: View {
                     HStack {
                         
                         NavigationLink {
-                        ReminderListView(reminders: scheduledReminders)
+                            ReminderListView(reminders: scheduledReminders)
                         } label: {
                             ReminderStatsView(icon: "calendar.circle.fill", title: "Scheduled", count: reminderStatsValues.scheduledCount, iconColor: .secondary)
                         }
-
+                        
                         NavigationLink {
-                        ReminderListView(reminders: completedReminders)
+                            ReminderListView(reminders: completedReminders)
                         } label: {
                             ReminderStatsView(icon: "checkmark.circle.fill", title: "Completed", count: reminderStatsValues.completedCount, iconColor: .primary)
                         }
-
+                        
                         
                         
                     }
@@ -100,39 +85,37 @@ struct ContentView1: View {
                         .bold()
                         .padding()
                     
-                MyListView(lists: lists)
-                       
-                       
-                Spacer()
-                
-                AddListButton(isPresented: $isPresented)
-                        
+                    MyListView(lists: lists)
                     
-                    .onChange(of: search, perform: { searchTerm in
-                       searching = !searchTerm.isEmpty ? true : false
+                    AddListButton(isPresented: $isPresented)
+                    
+                    
+                        .onChange(of: search, perform: { searchTerm in
+                            searching = !searchTerm.isEmpty ? true : false
                         })
                     
-                    .sheet(isPresented: $isPresented, content: {
-                        NavigationStack {
-                            AddNewListScreen()
-                        }
-                    })
+                        .sheet(isPresented: $isPresented, content: {
+                            NavigationStack {
+                                AddNewListScreen()
+                            }
+                        })
                     
-            }
+                }
             }
             .overlay(alignment: .center, content: {
-                ReminderListView(reminders: searchedReminders)
-                        .opacity(searching ? 1.0 : 0.0)
-                
-                
+                if searching {
+                    ReminderListView(reminders: searchedReminders)
+                        
+                }
+               
             })
-
             .onAppear {
                 reminderStatsValues = reminderStatsBuilder.build(reminders: reminders)
             }
             .navigationTitle("Reminders")
+            .searchable(text: $search, prompt: "Search reminders")
         }
-        .searchable(text: $search, prompt: "Search reminders")
+        
     }
 }
 
